1cb5cbaee12773a226dc5972f08aa9a57361c649
BadPotato1007 <neeravgupta07@gmail.com>
2024-08-08 15:26:24 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+from dash import Dash, html, Input, Output, callback
+import dash_daq as daq
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    daq.Knob(id='my-knob-1'),
+    html.Div(id='knob-output-1')
+])
+
+@callback(Output('knob-output-1', 'children'), Input('my-knob-1', 'value'))
+def update_output(value):
+    print(f'The knob value is: ', value)
+    return f'The knob value is {value}.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
